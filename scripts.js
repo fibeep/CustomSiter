2e846c6952bf04815ac4298038bde9f606201508
Salo <salomonco@gmail.com>
2020-10-16 13:29:42 -0400

    almost done with blog section

--- scripts.js
+++ scripts.js
@@ -1,4 +1,68 @@
-// This code modifies the Contact Form
+// This code modifies the Blog \\
+const blog = document.querySelector('.mainblog')
+const blogPic = document.querySelector("#blogpic")
+const blogHeader = document.querySelector("#blog-header")
+const blogPoem = document.querySelector("#blog-poem")
+const blogPar = document.querySelector("#poem-explained")
+const blogStyle = document.querySelector("#blog-pic-selector")
+const blogHeadFont = document.querySelector("#header-font")
+const blogHeaderColor = document.querySelector("#header-color")
+const blogTextFont = document.querySelector("#text-font")
+const blogTextColor = document.querySelector("#text-color")
+
+// Modifies Blog Style
+
+// Modifies header font
+blogHeadFont.addEventListener('input', blogHeaderFont)
+function blogHeaderFont() {
+    const newFont = blogHeadFont.value
+    blogHeader.style.fontFamily = newFont 
+}
+// Modifies header color
+blogHeaderColor.addEventListener('input', blogHeadColor)
+function blogHeadColor() {
+    const newColor = blogHeaderColor.value
+    blogHeader.style.color = newColor
+}
+// Modifies text font
+blogTextFont.addEventListener('input', newBlogTextFont)
+function newBlogTextFont() {
+    const newFont = blogTextFont.value
+    blogPar.style.fontFamily = newFont
+    blogPoem.style.fontFamily = newFont
+}
+// Modifies text color
+blogTextColor.addEventListener('input', newBlogTextColor)
+function newBlogTextColor(){
+    const newColor = blogTextColor.value
+    blogPar.style.color = newColor
+    blogPoem.style.color = newColor
+}
+// This code modifies the Shop \\
+const shop = document.querySelector("#main-shop")
+const shopItem = document.querySelectorAll(".shop-item")
+const shopType = document.querySelector("#shop-type-select")
+const buttonStyle = document.querySelector('#button-style')
+const roundedButton = document.querySelector("#rounded-button")
+const shopFont = document.querySelector("#shop-font")
+
+// Edits Shop Type
+
+// Edits Button Style
+
+// Option For Rounded Buttons
+
+// Edits Fonts - BROKEN NEEDS IMRPVEMENT
+// shopFont.addEventListener('input', editShopFont)
+// function editShopFont() {
+//     const newFont = shopFont.value
+//     shopItem.style.fontFamily = newFont
+// }
+
+// Shop Ends Here \\
+
+
+// This code modifies the Contact Form \\
 const form = document.querySelector("#contact-form")
 const formFontSize = document.querySelector("#form-text-size")
 const formFontColor = document.querySelector("#form-text-color")
@@ -10,33 +74,35 @@ const formBorderType = document.querySelector("#form-border-color")
 // Edits font size
 formFontSize.addEventListener('input', editFormFont)
 function editFormFont () {
-    newSize = formFontSize.value + 'px'
+    const newSize = formFontSize.value + 'px'
     form.style.fontSize = newSize
 }
 
-// Edits Font Color - BROKEN NEEDS REPAIR
+// Edits Font Color
 formFontColor.addEventListener('input', editFormColor)
 function editFormColor() {
-    newColor = formFontColor.value
+    const newColor = formFontColor.value
     form.style.color = newColor
 }
 
 // Edits Background Color
 formBgColor.addEventListener('input', editFormBg)
 function editFormBg() {
-    newBg = formBgColor.value
+    const newBg = formBgColor.value
     form.style.backgroundColor = newBg
 }
 
 // Edits Border Size ---> Needs improvement
-formBorderSize.addEventListener('input', editBorder)
-function editBorder() {
-    newBorder = formBorderSize.value + 'px'
-    form.style.borderWidth = newBorder
+// formBorderSize.addEventListener('input', editBorder)
+// function editBorder() {
+//     const newBorder = formBorderSize.value + 'px'
+//     form.style.borderWidth = newBorder
     
-    newColor = formBorderColor.value
-    form.style.borderColor = newColor
+//     const newColor = formBorderColor.value
+//     form.style.borderColor = newColor
 
-    newStyle  = formBorderType.value
-    form.style.borderStyle = newStyle
-}
+//     const newStyle  = formBorderType.value
+//     form.style.borderStyle = newStyle
+// }
+
+// Shop Customizations End Here \\
