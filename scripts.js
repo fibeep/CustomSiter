5590ddfffcb85f6b1a76df58f684fcdf2ed8f9c2
Salo <salomonco@gmail.com>
2020-10-18 16:26:10 -0400

    background picture editor works

--- scripts.js
+++ scripts.js
@@ -1,3 +1,26 @@
+// This code modifies the Home Picture Section \\
+const homeImg = document.querySelector('.home-main-img-text')
+const homeH1 = document.querySelector('#home-h1')
+const homeP = document.querySelector('#home-p')
+const homeBgImg = document.querySelector('#bg-img-url')
+const homeH1Font = document.querySelector('#h1-font-fam')
+const homeH1Color = document.querySelector('#h1-font-color')
+const homePText = document.querySelector('#par-text')
+const homePSpacing = document.querySelector('#p-spacing')
+const homePFont = document.querySelector('#p-font-fam')
+const homePColor = document.querySelector('#p-font-color')
+
+// Change background IMG
+homeBgImg.addEventListener('input', bgImg)
+function bgImg () {
+    const newBack = homeBgImg.value
+    homeImg.style.backgroundImage = "url(" + newBack + ")"
+    console.log(10)
+}
+
+// Home Pic Sec Done \\
+
+
 // This code modifies the Blog \\
 const blog = document.querySelector('.mainblog')
 const blogPic = document.querySelector("#blogpic")
